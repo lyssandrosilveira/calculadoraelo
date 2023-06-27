beb4258004c8ab325ee8821dbc72ab4062856c0f
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-27 09:41:57 -0300

    Update calculadora_elo_streamlit.py

--- calculadora_elo_streamlit.py
+++ calculadora_elo_streamlit.py
@@ -1,16 +1,12 @@
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
 
-$ streamlit run streamlit_app.py
-
-import streamlit as st
 
 def calculate_probability(ELO_mandante, ELO_visitante):
     p = (ELO_mandante / 400) / ((ELO_mandante / 400) + (ELO_visitante / 400))
     return p
 
-def run_app():
+
+def main():
     st.title("Cálculo da Probabilidade de Vitória")
     ELO_mandante = st.number_input("ELO do time mandante:")
     ELO_visitante = st.number_input("ELO do time visitante:")
@@ -25,8 +21,7 @@ def run_app():
     # Adicionar texto de desenvolvimento
     st.write("Desenvolvido por Lyssandro Silveira")
 
-def main():
-    run_app()
 
 if __name__ == '__main__':
     main()
+
